684b2021bfe5a10eb5c37ee40c9029d6c917786d
Charbel Salameh <csalameh131@gmail.com>
2026-07-09 09:00:05 +0300

    Added gitignore, removed unecessary files, prepared the code for render.com

--- form_countries/server.js
+++ form_countries/server.js
@@ -1,10 +1,12 @@
-https://github.com/CharbelSalameh/Internship_proj.gitlet 
 const express = require('express');
 const app = express();
 
-app.set('view engine','ejs');//to use the ejs files in the folder
+app.set('view engine', 'ejs');
+app.set('views',__dirname);//to use the ejs files in the folder
+
 app.use(express.static(__dirname));//to use the static files in the folder
 //get used when the user wants to get something from the server
+
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -13,10 +15,14 @@ app.get('/countries',function(req,res){
     res.sendFile(__dirname+ '/countries.html');
 });
 app.get('/profile/:name',function(req,res){
-    res.render(__dirname+ '/profile', { personName: req.params.name });
+    res.render('profile', { personName: req.params.name });
 });
 app.get('/profile',function(req,res){
-    res.render(__dirname+ '/profile', { personName: 'Anonymous' });
+    res.render('profile', { personName: 'Anonymous' });
 });
 
-app.listen(3000);//listen to a port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+    console.log("Server running on port " + PORT);
+});
